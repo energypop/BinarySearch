e40f46aa3fe4050074d6411f4abc2c45889fde30
Jinx <sashareconquista@proton.me>
2024-12-24 14:20:48 +0100

    Add project files.

---
+++ LetTheKitsuneGuideYou/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * This binary search will search the number inside a range
+ * given randomly
+*/
+
+using System;
+
+namespace LetTheKitsuneGuideYou
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+            Console.ReadKey();
+        }
+    }
+    
+    // This class contains the implementation of the algorithm 
+    internal class BinarySearch
+    {
+        public static void Search(int number)
+        {
+            // The number will be around 1 and 100
+            int minimum = 1;
+            int maximum = 100; 
+        }
+    }
+
+    // This class will save all the functions to generate a random number
+    internal class GuessingGame
+    {
+        public static int Randomize()
+        {
+            return 0;
+        }
+    }
+}
